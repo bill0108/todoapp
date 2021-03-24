add1203c96117c49f3072763716391467b5e0414
billy paniza <fhubil27@gmail.com>
2021-03-24 23:55:54 +0800

    add and delete

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -37,6 +37,13 @@ class TodosController <ApplicationController
 	def index
 	@todos=Todo.all
 	end
+	
+	def destroy
+	@todo=Todo.find (params[:id])
+	@todo.destroy
+	flash[:notice]="todo was successfully deleted"
+	redirect_to todos_path
+	end
   private
 	def todo_params
 	params.require(:todo).permit(:name,:description)
